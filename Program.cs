f0478b897b3ccc58f5874ddfa5994a2ce378147c
Joao Victor Carneiro Aureliano <T3637283@timbrasil.com.br>
2023-10-24 10:18:42 -0300

    beecrowd | 2147 | Galopeira

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) { 
+        
+        Console.Write("Quantidade de Casos: ");
+        var C = Convert.ToInt32(Console.ReadLine());
+
+        string[] Palavras = new string[C];
+        
+        for(int i = 0; i < C; i++)
+        {   
+            Console.Write("Digite a Palavra: ");
+            string? palavra = Console.ReadLine();
+            if(palavra.Length >= 9 && palavra.Length <= 10000)
+                Palavras[i] = palavra;
+            else
+                C = -1;
+        }   
+
+        for(int j = 0; j < Palavras.Length; j++)
+        {
+            if(Palavras[j] != null)
+            {
+                var T = (float)Palavras[j].Length / 100;
+                Console.WriteLine($"{T}");
+            }
+            
+        }
+    }   
+}
